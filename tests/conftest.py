fc98fcb75fe6fe4b1bd06fb06af98ec6a62a7be1
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-26 21:20:27 +0600

    Run alembic migration on starting test-session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from config import Config
 from models.utils import create_database_if_not_exists, drop_database_if_exists
+from alembic.config import Config as AlembicConfig
+from alembic import command
 
 
 def pytest_configure(config):
@@ -11,6 +13,8 @@ def pytest_sessionstart(session):
     drop_database_if_exists()
     create_database_if_not_exists()
 
+    alembic_config = AlembicConfig('alembic.ini')
+    command.upgrade(alembic_config, 'head')
 
 def pytest_sessionfinish(session):
     assert Config.db_url.endswith('_test')
